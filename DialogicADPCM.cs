1c5d6cc3bbd6d5c992c783c39e6c26d8ee6e650e
Universal-Networker <universalnetworker365@gmail.com>
2026-08-24 21:52:16 +0100

    Initial commit

---
+++ DialogicADPCM.cs
@@ -0,0 +1,106 @@
+﻿// Dialogic ADPCM encoder and decoder using the Dialogic ADPCM algorithm specification which is used by the OKI MSM6650/M6650.
+// Encodes PCM samples into Dialogic ADPCM samples.
+// Encodes Dialogic ADPCM samples into PCM samples.
+
+using System;
+using System.Collections.Generic;
+
+internal class DialogicADPCM
+{
+    int[] adjFactorTable = { -1, -1, -1, -1, 2, 4, 6, 8, -1, -1, -1, -1, 2, 4, 6, 8 };
+    int[] stepSizeTable = { 16, 17, 19, 21, 23, 25, 28, 31, 34, 37, 41, 45, 50, 55, 60, 66, 73, 80, 88, 97, 107, 118, 130, 143, 157, 173, 190, 209, 230, 253, 279, 307, 337, 371, 408, 449, 494, 544, 598, 658, 724, 796, 876, 963, 1060, 1166, 1282, 1411, 1552 };
+    int stepSize = 0;
+    int stepSizeIndex = 0;
+    int predictedSample = 0;
+
+    int lastPCM12sample = 0;
+
+    List<int> adpcmSamples = new List<int>();
+    List<int> PCMsamples = new List<int>();
+    public int decodeSample(int nibble)
+    {
+        stepSize = stepSizeTable[stepSizeIndex];
+
+        int B3 = (nibble >> 3) & 1;
+        int B2 = (nibble >> 2) & 1;
+        int B1 = (nibble >> 1) & 1;
+        int B0 = (nibble >> 0) & 1;
+
+        int dif = (stepSize * B2) + ((stepSize >> 1) * B1) + ((stepSize >> 2) * B0) + (stepSize >> 3);
+        if (B3 == 1)
+        {
+            dif = dif * (-1);
+        }
+        predictedSample = predictedSample + dif;
+
+        if (predictedSample > 2047)
+        {
+            predictedSample = 2047;
+        }
+        if (predictedSample < -2048)
+        {
+            predictedSample = -2048;
+        }
+
+        int PCM16sample = predictedSample << 4;
+
+        int adjFactor = adjFactorTable[nibble];
+        stepSizeIndex = stepSizeIndex + adjFactor;
+        if (stepSizeIndex > 48)
+        {
+            stepSizeIndex = 48;
+        }
+        if (stepSizeIndex < 0)
+        {
+            stepSizeIndex = 0;
+        }
+
+        return PCM16sample;
+    }
+    public int encodeSample(int PCM16sample)
+    {
+        int PCM12sample = PCM16sample >> 4;
+        int dif = PCM12sample - lastPCM12sample;
+        lastPCM12sample = PCM12sample;
+        stepSize = stepSizeTable[stepSizeIndex];
+
+        int B3 = 0;
+        int B2 = 0;
+        int B1 = 0;
+        int B0 = 0;
+
+        if (dif < 0)
+        {
+            B3 = 1;
+        }
+        dif = Math.Abs(dif);
+        if (dif >= stepSize)
+        {
+            B2 = 1;
+            dif = dif - stepSize;
+        }
+        if (dif >= stepSize >> 1)
+        {
+            B1 = 1;
+            dif = dif - (stepSize >> 1);
+        }
+        if (dif >= stepSize >> 2)
+        {
+            B0 = 1;
+        }
+        int ADPCMsample = (B3 << 3) | (B2 << 2) | (B1 << 1) | B0;
+
+        int adjFactor = adjFactorTable[ADPCMsample];
+        stepSizeIndex = stepSizeIndex + adjFactor;
+        if (stepSizeIndex > 48)
+        {
+            stepSizeIndex = 48;
+        }
+        if (stepSizeIndex < 0)
+        {
+            stepSizeIndex = 0;
+        }
+
+        return ADPCMsample;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,226 @@
+﻿using Microsoft.VisualBasic;
+using System.Text;
+using NAudio.Wave;
+using NAudio.Lame;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Error: No arguments provided.");
+            Console.WriteLine("MSM6650Decoder.exe - A tool to decode MSM6650 ADPCM binaries into PCM audio.");
+            Console.WriteLine("Usage: MSM6650Decoder.exe <args>");
+            Console.WriteLine("Args:");
+            Console.WriteLine("Input Binaries    : -i \"1.bin,2.bin,3.bin,4.bin etc\" : Required (Input binaries must be in same directory and should be entered in a comma seperated list with no spaces.)");
+            Console.WriteLine("Input Sample Rate : -s \"8/16 etc\"                    : Required (Number in Khz.)");
+            Console.WriteLine("Input Sample Rate : -f \"WAV/MP3 etc\"                 : Optional (WAV or MP3, defaults to WAV.)");
+            Console.WriteLine("Output Directory  : -o \"Output Directory\"            : Optional (Defaults to current directory.)");
+            return;
+        }
+
+        List<string> inputBinaries = new List<string>();
+        int sampleRate = 0;
+        string outputFormat = "WAV";
+        string outputDirectory = Directory.GetCurrentDirectory();
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            switch (args[i])
+            {
+                case "-i":
+                    if (i + 1 < args.Length)
+                    {
+                        inputBinaries.AddRange(args[++i].Split(','));
+                    }
+                    break;
+
+                case "-s":
+                    if (i + 1 < args.Length)
+                    {
+                        sampleRate = int.Parse((float.Parse(args[++i]) * 1000).ToString());
+                    }
+                    break;
+
+                case "-f":
+                    if (i + 1 < args.Length)
+                    {
+                        outputFormat = args[++i].ToUpper();
+                    }
+                    break;
+
+                case "-o":
+                    if (i + 1 < args.Length)
+                    {
+                        outputDirectory = args[++i];
+                        if (!Directory.Exists(outputDirectory))
+                        {
+                            Directory.CreateDirectory(outputDirectory);
+                        }
+                    }
+                    break;
+
+                case "-h":
+                case "--help":
+                    Console.WriteLine("MSM6650Decoder.exe - A tool to decode MSM6650 ADPCM binaries into PCM audio.");
+                    Console.WriteLine("Usage: MSM6650Decoder.exe <args>");
+                    Console.WriteLine("Args:");
+                    Console.WriteLine("Input Binaries    : -i \"1.bin,2.bin,3.bin,4.bin etc\" : Required (Input binaries must be in same directory and should be entered in a comma seperated list with no spaces.)");
+                    Console.WriteLine("Input Sample Rate : -s \"8/16 etc\"                    : Required (Number in Khz.)");
+                    Console.WriteLine("Input Sample Rate : -f \"WAV/MP3 etc\"                 : Optional (WAV or MP3, defaults to WAV.)");
+                    Console.WriteLine("Output Directory  : -o \"Output Directory\"            : Optional (Defaults to current directory.)");
+                    return;
+            }
+        }
+
+        if(inputBinaries.Count == 0)
+        {
+            Console.WriteLine("Error: Input binaries not provided. Use -i to specify the input binaries in a comma seperated list with no spaces.");
+            return;
+        }
+        if (sampleRate == 0)
+        {
+            Console.WriteLine("Error: Input sample rate not provided. Use -s to specify the input sample rate in Khz.");
+            return;
+        }
+
+        byte[] combinedBinary = combineBinaries(inputBinaries);
+
+        DecodeSegments(combinedBinary, sampleRate, outputFormat, outputDirectory);
+    }
+
+    private static void DecodeSegments(byte[] combinedBinary, int sampleRate, string outputFormat, string outputDirectory)
+    {
+        List<List<int>> segments = new List<List<int>>();
+        List<int> segmentPosValues = new List<int>();
+        int segmentPos = 0;
+        int pos = 0x800;
+        while (true)
+        {
+            if (combinedBinary[pos] == 0)
+            {
+                pos += 4;
+            }
+            else
+            {
+                if(combinedBinary[pos] == 0x07)
+                {
+                    int value = (combinedBinary[pos + 1] << 16) | (combinedBinary[pos + 2] << 8) | combinedBinary[pos + 3];
+                    List<int> decodedSamples = DecodeSeg(value, combinedBinary);
+                    segments.Add(decodedSamples);
+                    segmentPosValues.Add(segmentPos);
+                    pos += 4;
+                }
+            }
+            segmentPos++;
+            if(pos >= 0xA00)
+            {
+                break;
+            }
+        }
+
+        for (int i = 0; i < segments.Count; i++)
+        {
+            List<int> segment = segments[i];
+            int segmentPosValue = segmentPosValues[i];
+            string outputFileName = Path.Combine(outputDirectory, $"Segment_{segmentPosValue}.{outputFormat.ToLower()}");
+            if (outputFormat == "WAV")
+            {
+                ExportWav(segment, sampleRate, outputFileName);
+            }
+            else if (outputFormat == "MP3")
+            {
+                ExportMp3(segment, sampleRate, outputFileName);
+            }
+            else
+            {
+                Console.WriteLine($"Error: Unsupported output format '{outputFormat}'. Supported formats are WAV and MP3.");
+                return;
+            }
+        }
+        Console.WriteLine($"Decoding complete. {segments.Count} segments decoded and saved to '{outputDirectory}'.");
+    }
+
+    static void ExportWav(List<int> samples, int sampleRate, string filename)
+    {
+        var format = new WaveFormat(sampleRate, 16, 1);
+
+        using (var writer = new WaveFileWriter(filename, format))
+        {
+            foreach (int sample in samples)
+            {
+                writer.WriteByte((byte)(sample & 0xFF));
+                writer.WriteByte((byte)((sample >> 8) & 0xFF));
+            }
+        }
+    }
+
+    static void ExportMp3(List<int> samples, int sampleRate, string filename)
+    {
+        var format = new WaveFormat(sampleRate, 16, 1);
+
+        using var writer = new LameMP3FileWriter(
+            filename,
+            format,
+            LAMEPreset.STANDARD);
+
+        foreach (int sample in samples)
+        {
+            short pcm = (short)Math.Clamp(sample, short.MinValue, short.MaxValue);
+
+            writer.WriteByte((byte)(pcm & 0xFF));
+            writer.WriteByte((byte)((pcm >> 8) & 0xFF));
+        }
+    }
+
+    private static List<int> DecodeSeg(int startingPos, byte[] combinedBinary)
+    {
+        DialogicADPCM adpcmDecoder = new DialogicADPCM();
+        List<int> decodedSamples = new List<int>();
+
+        int pos = startingPos;
+        int blockByte = 0xFF;
+
+        while (blockByte != 0)
+        {
+            blockByte = combinedBinary[pos];
+            int count = 0;
+
+            while (count < blockByte)
+            {
+                pos++;
+
+                byte currentByte = combinedBinary[pos];
+
+                int upperNibble = (currentByte >> 4) & 0x0F;
+                int lowerNibble = currentByte & 0x0F;
+
+                decodedSamples.Add(adpcmDecoder.decodeSample(upperNibble));
+                decodedSamples.Add(adpcmDecoder.decodeSample(lowerNibble));
+
+                count++;
+            }
+
+            pos++;
+        }
+
+        return decodedSamples;
+    }
+
+    private static byte[] combineBinaries(List<string> inputBinaries)
+    {
+        List<byte> combinedBinary = new List<byte>();
+        foreach (string binaryFile in inputBinaries)
+        {
+            if (!File.Exists(binaryFile))
+            {
+                Console.WriteLine($"Error: Input binary file '{binaryFile}' does not exist.");
+                continue;
+            }
+            byte[] binaryData = File.ReadAllBytes(binaryFile);
+            combinedBinary.AddRange(binaryData);
+        }
+        return combinedBinary.ToArray();
+    }
+}
